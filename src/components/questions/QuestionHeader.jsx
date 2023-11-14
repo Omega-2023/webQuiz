bb75fe31be767f33b2f226a4b236206af97b7450
AsumVictor <Iamasum369@gmail.com>
2023-11-14 18:14:27 +0000

    Change bg

--- src/components/questions/QuestionHeader.jsx
+++ src/components/questions/QuestionHeader.jsx
@@ -2,12 +2,15 @@ import Timimg from "../timing/timingBox.jsx";
 
 function QuestionHeader() {
   return (
-    <div className="h-[1.7cm] bg-gray-600 w-full flex flex-row justify-between items-center px-3">
+    <div className="h-[1.7cm] bg-dark-700 w-full flex flex-row justify-between items-center px-3">
       <div className=" font-medium text-2xl text-gray-300"> Examplify</div>
       <div>
         <Timimg />
       </div>
-      <div className=" hidden 450px:block font-medium text-2xl text-gray-300"> ToolKit : </div>
+      <div className=" hidden 450px:block font-medium text-2xl text-gray-300">
+        {" "}
+        ToolKit :{" "}
+      </div>
     </div>
   );
 }
